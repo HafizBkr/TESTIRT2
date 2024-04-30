629bb69ff8558191bf423f6a77efaa745e5a366c
blackCAT14-CLOUD <hafizinovus@gmail.com>
2024-05-01 00:14:40 +0200

    commit pour la fonction d'adition

--- main.go
+++ main.go
@@ -3,6 +3,14 @@ package main
 import (
 	"fmt"
 )
+
+   func add(a int , b int){
+	var c int
+       c = a+b
+	   fmt.Print("le resultat est ", c)
+   }
+
 func main (){
-	fmt.Print("HELLO IRT2")
+	fmt.Print("HELLO IRT2 \n")
+	add(5,6)
 }
