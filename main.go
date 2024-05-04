961ce64313f69e7fde107222b2d5ccd1785a30d9
blackCAT14-CLOUD <hafizinovus@gmail.com>
2024-05-04 23:08:47 +0200

    commit sur la fonction addd

--- main.go
+++ main.go
@@ -10,7 +10,14 @@ import (
 	   fmt.Print("le resultat est ", c)
    }
 
+   func addd(b int , c int){
+	var d int
+       d = b+c
+	   fmt.Print("le resultat est \n", d)
+   }
+
 func main (){
 	fmt.Print("HELLO IRT2 \n")
 	add(5,6)
+	addd(19,2)
 }
